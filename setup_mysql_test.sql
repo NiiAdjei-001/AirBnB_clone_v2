a1971b90f9a72b9685b161a745cea1bea581b4e3
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2024-06-11 03:39:08 +0000

    setup_mysql_test.sql generated

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,14 @@
--- Setup MySQL setup for development
--- Create database hbnb_dev_db
+-- Setup MySQL setup for test
+-- Create database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Create user hbnb_dev
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
 IDENTIFIED BY 'hbnb_test_pwd';
--- Grant all privileges to user on hbnb_dev_db
+-- Grant all privileges to user on hbnb_test_db
 GRANT ALL PRIVILEGES
-ON hbnb_dev_db.*
+ON hbnb_test_db.*
 TO 'hbnb_test'@'localhost';
--- Grant all privileges to user on hbnb_dev_db
+-- Grant all privileges to user on hbnb_test_db
 GRANT SELECT
 ON performance_schema.*
 TO 'hbnb_test'@'localhost';
